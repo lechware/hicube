361fa89771ca82b503c0977eaa27ee00aec33f44
Gaurav Cheema <gcheema@tecfoundary.com>
2015-04-30 14:56:14 +1000

    Fix - content tag should query unscoped pages

--- lib/hicube/liquid/tags/content.rb
+++ lib/hicube/liquid/tags/content.rb
@@ -11,7 +11,7 @@ module Hicube
         def render(context)
           return "<div class=\"alert alert-danger\" role=\"alert\">Supply page and content names in content_tag</div>" if @variables.split(" ").count < 2
 
-          @page = Hicube::Page.find(@variables.split(" ").first) 
+          @page = Hicube::Page.unscoped.find(@variables.split(" ").first) 
           @content = @page.content.find(@variables.split(" ").last)
       
           "<div class=\"editable\" data-type=\"wysihtml5\" data-name=\"content[body]\" data-url=\"/hicube/pages/#{@page.slug}/contents/#{@content.slug}\">#{@content.body}</div>\n"
